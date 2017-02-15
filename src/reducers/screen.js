110f2c745d9093409f933dd3d0aaa61d76c15edd
corynorris <corynorris@gmail.com>
2017-02-15 15:49:54 -0500

    added reducer for screne position

---
+++ src/reducers/screen.js
@@ -0,0 +1,21 @@
+
+const screenInitialState = {
+  top: 0,
+  left: 0,
+}
+
+export const screen = (state = screenInitialState, action) => {
+  switch (action.type) {
+    case 'SET_SCREEN_OFFSET':
+      return Object.assign(
+        {}, state, {
+          top: action.top,
+          left: action.left
+        }
+      )
+    default:
+      return state
+  }
+}
+
+export default screen
